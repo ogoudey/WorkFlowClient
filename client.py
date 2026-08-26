a6d7b6eccce2aa10c7cb95f545a1f480b25d9f36
Olin Goudey <olin@labyrinthai.co>
2026-08-26 10:56:38 -0400

    tested

--- client.py
+++ client.py
@@ -2,7 +2,36 @@ import boto3
 import json
 import sys
 
-request = sys.argv[1]
+saved_payload = {
+    "workflow_id": "test_workflow_4uf83u39",
+    "blocks": [
+        {
+            "name": "sync_to_30",
+            "command": {
+                "source_bucket": "s3://amzn-s3-log-bucket-laby-test513270157",
+                "destination": "s3://amzn-s3-log-bucket-laby-output125351"
+            }
+        }
+    ]
+}
+
+if len(sys.argv) > 1:
+    request = sys.argv[1]
+else:
+    print("Using default test request. To use a custom request, run `python3 client.py '<request_json>'`")
+    payload = {
+        "workflow_id": "test_composite",
+        "blocks": [
+            {
+                "name": "composite_videos",
+                "command": {
+                    "source_bucket": "s3://amzn-s3-log-bucket-laby-test513270157",
+                    "destination": "s3://amzn-s3-log-bucket-laby-output125351"
+                }
+            }
+        ]
+    }
+    request = json.dumps(payload)
 
 ecs = boto3.client("ecs", region_name="us-east-2")
 
